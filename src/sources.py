85031ddde787eac885912441c414996c0b10ed85
sunshinelily66-a11y <sunshinelily66@gmail.com>
2026-01-22 15:21:28 +0800

    Create sources.py

---
+++ src/sources.py
@@ -0,0 +1,26 @@
+SOURCES = [
+    {
+        "name": "InsideEVs",
+        "type": "rss",
+        "url": "https://insideevs.com/rss/news/",
+        "category": "电动车/行业"
+    },
+    {
+        "name": "Electrek",
+        "type": "rss",
+        "url": "https://electrek.co/feed/",
+        "category": "电动车/科技"
+    },
+    {
+        "name": "TechCrunch Transportation",
+        "type": "rss",
+        "url": "https://techcrunch.com/tag/transportation/feed/",
+        "category": "出行/投融资"
+    },
+    {
+        "name": "The Verge Transportation",
+        "type": "rss",
+        "url": "https://www.theverge.com/transportation/rss/index.xml",
+        "category": "出行/产品"
+    },
+]
